913b1753095ca1df0e9a6b8be026c429e6569ab4
Jan Lebert <jan.lebert@ucsf.edu>
2023-10-26 16:56:52 -0700

    Breaking: activation map is now in milliseconds and show=True by default
    Previously it was in seconds if fps parameter was given.

--- optimap/activation/_core.py
+++ optimap/activation/_core.py
@@ -1,12 +1,15 @@
 import numpy as np
 
 from ..utils import _print, print_bar
+from ..image import show_image
 
-def compute_activation_map(video, threshold=0.5, inverted=False, fps=None, set_nan_for_inactive=True):
+def compute_activation_map(video, threshold=0.5, inverted=False, fps=None, set_nan_for_inactive=True, show=True, cmap='jet'):
     """
     Computes an activation map (or isochrone map) from a given video based on pixel intensity thresholding.
     
     For each pixel in the video, the function determines the time (or frame index) at which the pixel's intensity first surpasses (or falls below, if inverted is set to True) the specified threshold.
+
+    If `fps` is specified, time is giving in milliseconds, otherwise, it is given in frames.
     
     Parameters
     ----------
@@ -18,10 +21,14 @@ def compute_activation_map(video, threshold=0.5, inverted=False, fps=None, set_n
         If True, the function will compute the time/frame when pixel intensity falls below the threshold, 
         rather than surpassing it. Defaults to False.
     fps : float, optional
-        If provided, the resulting activation map will represent times in seconds based on this frame rate,
+        If provided, the resulting activation map will represent times in milliseconds based on this frame rate,
         otherwise, it will be in frames.
     set_nan_for_inactive : bool, optional
         If True, pixels that never reach the activation threshold will be set to NaN. Defaults to True.
+    show : bool, optional
+        If True, the resulting activation map will be displayed. Defaults to True.
+    cmap : str, optional
+        Colormap to use for displaying the activation map. Defaults to 'jet'.
     
     Returns
     -------
@@ -39,7 +46,7 @@ def compute_activation_map(video, threshold=0.5, inverted=False, fps=None, set_n
     
     amap = amap.astype(np.float32)
     if fps is not None:
-        amap = amap / fps
+        amap = amap * 1000.0 / fps
 
     if set_nan_for_inactive:
         # set all pixels to NaN that never reach threshold
@@ -55,4 +62,9 @@ def compute_activation_map(video, threshold=0.5, inverted=False, fps=None, set_n
     _print(f'minimum of activation_map: {np.nanmin(amap)}')
     _print(f'maximum of activation_map: {np.nanmax(amap)}')
     print_bar()
+
+    if show:
+        cbar_label = 'Activation Time [ms]' if fps is not None else 'Activation Time [frames]'
+        show_image(amap, cmap=cmap, show_colorbar=True, title='Activation Map', colorbar_title=cbar_label)
+
     return amap
