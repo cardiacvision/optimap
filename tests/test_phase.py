b2367bd6a521993c090466876d127ca397bf73d5
Jan Lebert <jan.lebert@ucsf.edu>
2023-10-26 17:05:32 -0700

    Add basic phase test (sanity check only for now)

---
+++ tests/test_phase.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+import optimap as om
+
+def test_compute_phase():
+    video = np.random.random((100, 10, 10)).astype(np.float32)
+    phase = om.phase.compute_phase(video)
+    assert phase.shape == video.shape
+    assert phase.dtype == np.float32
+    assert np.allclose(phase.min(), -np.pi, atol=0.2)
+    assert np.allclose(phase.max(), np.pi, atol=0.2)
