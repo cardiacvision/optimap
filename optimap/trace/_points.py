247583291e59cb898927d5a009c0dac71bd721fc
Jan Lebert <jan.lebert@ucsf.edu>
2024-02-15 15:52:57 -0800

    Fix om.trace.random_positions()

--- optimap/trace/_points.py
+++ optimap/trace/_points.py
@@ -1,8 +1,6 @@
 import numpy as np
 import skimage
 
-from ..utils import _print, print_bar
-
 
 def random_positions(shape_or_mask, N=1):
     """Returns N randomly distributed positions within image shape or mask.
@@ -17,37 +15,32 @@ def random_positions(shape_or_mask, N=1):
     Returns
     -------
     list of tuples
-        TODO: (x, y) or (y, x) ???
+        list of N positions (y, x)
     """
-    mask = None
     if isinstance(shape_or_mask, np.ndarray):
-        mask = shape_or_mask
-        Nx, Ny = mask.shape
-        _print(f"creating {N} random (2D) positions within mask")
+        mask = shape_or_mask.astype(bool)
+        shape = mask.shape
     else:
-        Nx, Ny = shape_or_mask
-        _print(f"creating {N} random (2D) positions within (0,0) and ({Nx}, {Ny})")
-
-    points = []
-    while len(points) < N:
-        x = np.random.randint(Nx)
-        y = np.random.randint(Ny)
-        if (x, y) in points:
-            continue
-
-        # TODO: this is not very efficient, but works for now
-        if mask is not None:
-            if mask[x, y]: #TODO (y, x)?
-                points.append((x, y))
-        else:
-            points.append((x, y))
-    points = np.asarray(points)
+        mask = np.full(shape_or_mask, True, dtype=bool)
+        shape = shape_or_mask
+    mask = mask / mask.sum()
+    
+    rng = np.random.default_rng()
+    idxs = rng.choice(
+        np.arange(mask.size),
+        p=mask.flatten(),
+        size=N,
+        replace=False
+    )
+    points = np.unravel_index(idxs, shape)
     points = np.transpose(points)
-    print_bar()
-    return points
+    points = points[:, ::-1]
+    return points.tolist()
+
 
 def line_positions(start, end):
     return skimage.draw.line(start[0], start[1], end[0], end[1]) #TODO (y, x)?
+
 
 def positions_from_A_to_B(N, A, B):
     """Creates N positions along straight line from A to B (2D coordinates).
