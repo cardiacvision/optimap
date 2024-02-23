42385718dbd0d2dfcb22a3e15112d516828fdac7
Jan Lebert <jan.lebert@ucsf.edu>
2024-02-23 15:59:53 -0800

    Rename retrieve_example_data to download_example_data

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,8 +5,8 @@ import numpy as np
 import optimap as om
 
 
-def test_retrieve_sample():
-    filename = om.utils.retrieve_example_data("optimap-test-download-file.npy")
+def test_download_example_data():
+    filename = om.utils.download_example_data("test-download-file.npy")
     assert Path(filename).exists()
     assert Path(filename).is_file()
     assert Path(filename).suffix == ".npy"
