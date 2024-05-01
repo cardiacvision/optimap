b9623281635f6ab24beed70b53060a6c149553ec
Jan Christoph <jan.christoph@ucsf.edu>
2024-05-01 16:09:08 -0700

    added links to example files 02 and 04

--- optimap/utils.py
+++ optimap/utils.py
@@ -43,6 +43,8 @@ FILE_HASHES = {
         "sha256:a1781582b669a69a9753b1c43d23e0acf26fb372426eeb6d880d2e66420b2107",
     "Example_02_VF_Rabbit_Di-4-ANEPPS_Basler_acA720-520um_warped_mask.npy":
         "sha256:3f5d8402c8251f3cb8e8d235b459264ff7e7e2cf2b81f08129f0897baa262db6",
+    "optimap-example-file-02.npy":
+        "https://cardiacvision.ucsf.edu/sites/g/files/tkssra6821/f/optimap-example-file-02.npy_.webm",
     "optimap-example-file-03.npy":
         "https://cardiacvision.ucsf.edu/sites/g/files/tkssra6821/f/optimap-example-file-03.npy_.webm",
     "optimap-example-file-04.npy":
